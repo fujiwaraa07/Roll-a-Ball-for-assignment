70e00fd47555494119aa76e185e9b0332085631b
fujiwaraa07 <fujiwaraayumu15@gmail.com>
2024-12-12 09:42:44 +0900

    ブレーキ機能の実装は一旦完了

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,26 +7,28 @@ public class PlayerController : MonoBehaviour
     // speedを制御する
     public float speed = 10;
     private PhysicMaterial physicMaterial;
+    private new Rigidbody rigidbody;
     float dynamicFriction_base;
+    float mass_base;
+    float drag_base;
+    float brake_power;
     
     // Start is called before the first frame update
     void Start()
     {
         physicMaterial = GetComponent<Collider>().material;
+        rigidbody = GetComponent<Rigidbody>();
         dynamicFriction_base = physicMaterial.dynamicFriction;
+        mass_base = rigidbody.mass;
+        drag_base = rigidbody.drag;
         Debug.Log("Hello World");
-        Debug.Log("Base Friction = " + physicMaterial.dynamicFriction);
+        Debug.Log("Base Friction = " + physicMaterial.dynamicFriction + ", Base Mass = " + rigidbody.mass);
     }
 
     void Update()
     {
-        // Spaceキーが押されたらブレーキをかける
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("Space key was pressed.");
-            Debug.Log("Friction = " + physicMaterial.dynamicFriction);
-            physicMaterial.dynamicFriction = 1.0f;
-        }
+        // Debug.Log("Friction = " + physicMaterial.dynamicFriction);
+        
     }
 
     void FixedUpdate()
@@ -36,10 +38,26 @@ public class PlayerController : MonoBehaviour
         float z = Input.GetAxis("Vertical");
         // Debug.Log("x = " + x + ", z = " + z);
 
-        // 同一のGameObjectが持つRigidbodyコンポーネントを取得
-        Rigidbody rigidbody = GetComponent<Rigidbody>();
 
         // rigidbodyのx軸（横）とz軸（奥）に力を加える
         rigidbody.AddForce(x * speed, 0, z * speed);
+        
+        // Spaceキーが押されている間はブレーキをかける
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space key was pressed.");
+            rigidbody.mass = mass_base * 100.0f;
+            rigidbody.drag = 50.0f;
+            physicMaterial.dynamicFriction = 1.0f;
+            brake_power = -1.0f * rigidbody.mass * physicMaterial.dynamicFriction;
+            rigidbody.AddForce(brake_power * speed, 0, brake_power * speed);
+            Debug.Log("Friction = " + physicMaterial.dynamicFriction + ", Mass = " + rigidbody.mass + ", Drag = " + rigidbody.drag);
+        }
+        else
+        {
+            physicMaterial.dynamicFriction = dynamicFriction_base;
+            rigidbody.mass = mass_base;
+            rigidbody.drag = drag_base;
+        }
     }
 }
