bf2f88708398e344166363f3b450ad2cbd2233d8
fujiwaraa07 <fujiwaraayumu15@gmail.com>
2024-12-12 09:16:51 +0900

    Assets\Script\PlayerController.csにブレーキ機能を追加

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,11 +6,27 @@ public class PlayerController : MonoBehaviour
 {
     // speedを制御する
     public float speed = 10;
+    private PhysicMaterial physicMaterial;
+    float dynamicFriction_base;
     
     // Start is called before the first frame update
     void Start()
     {
+        physicMaterial = GetComponent<Collider>().material;
+        dynamicFriction_base = physicMaterial.dynamicFriction;
         Debug.Log("Hello World");
+        Debug.Log("Base Friction = " + physicMaterial.dynamicFriction);
+    }
+
+    void Update()
+    {
+        // Spaceキーが押されたらブレーキをかける
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space key was pressed.");
+            Debug.Log("Friction = " + physicMaterial.dynamicFriction);
+            physicMaterial.dynamicFriction = 1.0f;
+        }
     }
 
     void FixedUpdate()
@@ -18,7 +34,7 @@ public class PlayerController : MonoBehaviour
         // 入力をxとzに代入
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        Debug.Log("x = " + x + ", z = " + z);
+        // Debug.Log("x = " + x + ", z = " + z);
 
         // 同一のGameObjectが持つRigidbodyコンポーネントを取得
         Rigidbody rigidbody = GetComponent<Rigidbody>();
