38bb074c196ca4bce6d86a74ef30b275b8b42160
fujiwaraa07 <fujiwaraayumu15@gmail.com>
2024-12-12 11:50:23 +0900

    ステージ拡張完了

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -6,17 +6,21 @@ public class GameController : MonoBehaviour
 {
     public UnityEngine.UI.Text scoreLabel;
     public GameObject winnerLabelObject;
+
+    private int maxItemCount;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxItemCount = GameObject.FindGameObjectsWithTag ("Item").Length;
+        winnerLabelObject.SetActive (false);
     }
 
     // Update is called once per frame
     void Update()
     {
         int count = GameObject.FindGameObjectsWithTag ("Item").Length;
-        scoreLabel.text = count.ToString();
+        scoreLabel.text = count.ToString() + " / " + maxItemCount.ToString();
 
         if (count == 0)
         {
