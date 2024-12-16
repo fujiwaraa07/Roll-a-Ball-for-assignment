95c3d9e3b93e584b52e8a016a344c42c4ca294a4
fujiwaraa07 <fujiwaraayumu15@gmail.com>
2024-12-17 03:40:38 +0900

    final commit

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -7,7 +7,7 @@ public class Item : MonoBehaviour
     // トリガーとの接触時に呼ばれるコールバック
     void OnTriggerEnter(Collider hit)
     {
-        // 接触対象はPlayerタグですか？
+        // 接触対象はPlayerタグか
         if (hit.CompareTag("Player"))
         {
             // このコンポーネントを持つGameObjectを破棄する
